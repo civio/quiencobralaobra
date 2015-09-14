cff8aa90a6ceda3ccdbe585870c631a74457f85b
Raúl Díaz Poblete <info@rauldiazpoblete.com>
2015-09-14 15:39:23 +0200

    Add responsive styles & title icons

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,9 +1,16 @@
 //= require_tree .
 //= require bootstrap
 
+var $cont,
+    contWidth = 0;
+
 $( document ).ready(function() {
     
   console.log( "ready!" );
+
+  // Store cont & cont width
+  $cont = $('body > .container');
+  contWidth = $cont.width();
 
   // Add sort capability to tables
   $('.footable').footable();
@@ -21,4 +28,21 @@ $( document ).ready(function() {
       });
     });
   }
+
+  // Resize Handler
+  $(window).bind('resize', function(){
+
+    if ($cont && contWidth !== $cont.width()) {
+
+      contWidth = $cont.width();
+
+      // Update packery column width
+      if ($wall.length) {
+        $wall.packery({
+          itemSelector: '.wall-item',
+          columnWidth: $wall.width() / 12
+        });
+      }
+    }
+  });
 });
