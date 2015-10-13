1c578db063e03fea03f2276ddfdfa2b54b033ada
Raúl Díaz Poblete <info@rauldiazpoblete.com>
2015-10-13 16:59:12 +0200

    Remove Add This
    Closes #3

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,6 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+# Define Facebook App Id for Facebook Sharing Buttons
+Rails.configuration.facebook_app_id = 400262566836912
