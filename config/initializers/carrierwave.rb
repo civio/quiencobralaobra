1864111a470047d8aab5fd4d2e645f7a50eed6bc
David Cabo <david.cabo@gmail.com>
2016-01-05 03:57:53 +0100

    Remove Heroku references

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,9 +3,6 @@ CarrierWave.configure do |config|
     # Configuration for Amazon S3 should be made available through an Environment variable.
     # For local installations, export the env variable through the shell OR
     # if using Passenger, set an Apache environment variable.
-    # In Heroku, follow http://devcenter.heroku.com/articles/config-vars
-    #
-    # $ heroku config:add S3_KEY=your_s3_access_key S3_SECRET=your_s3_secret S3_REGION=eu-west-1 S3_BUCKET_NAME=s3_bucket/folder
     :provider               => 'AWS',                           # required
     :aws_access_key_id      => ENV['S3_KEY'],                   # required
     :aws_secret_access_key  => ENV['S3_SECRET'],                # required
@@ -29,5 +26,5 @@ CarrierWave.configure do |config|
     config.storage = :fog
   end
    
-  config.cache_dir = "#{Rails.root}/tmp/uploads" # To let CarrierWave work on heroku
+  config.cache_dir = "#{Rails.root}/tmp/uploads"
 end
