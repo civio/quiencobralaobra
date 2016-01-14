21afcfecb788bbcfc461d56de9a000834b0a0cb1
David Cabo <david.cabo@gmail.com>
2016-01-14 14:02:47 +0100

    Parse amount correctly and store in cents

--- app/models/award.rb
+++ app/models/award.rb
@@ -1,3 +1,5 @@
+require 'bigdecimal'
+
 class Award < ActiveRecord::Base
   belongs_to :public_body
   belongs_to :bidder
@@ -8,8 +10,15 @@ class Award < ActiveRecord::Base
     # whether we're doing it correctly.
     # return unless properties['[QCLO] Es Obra Pública']=='S'
 
+    # Find or create related entities
     public_body = PublicBody.where(name: properties['Entidad adjudicadora - Organismo']).first_or_create
     bidder = Bidder.where(name: properties['Formalización del contrato - Contratista']).first_or_create
+
+    # Read amount and store it in cents. We are being extra careful here not to change any amount
+    # even in one cent. That's why we are avoiding floats, not a good fit when exact precision is needed.
+    amount = BigDecimal.new(properties['Análisis - Importe'])*100
+
+    # Store the award
     Award.create!({
         public_body: public_body,
         bidder: bidder, 
@@ -17,7 +26,7 @@ class Award < ActiveRecord::Base
         category: properties['Análisis - Tipo'],
         process_type: properties['Análisis - Procedimiento'],
         process_track: properties['Análisis - Tramitación'],
-        amount: properties['Análisis - Importe']*100, # in cents
+        amount: amount,
         properties: properties
       })
   end
