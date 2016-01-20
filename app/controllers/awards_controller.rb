c3d2e2f72f03450c2224f0094929d53c1a4cb534
Raúl Díaz Poblete <info@rauldiazpoblete.com>
2016-01-20 16:54:55 +0100

    Add 'contratos' filter form

--- app/controllers/awards_controller.rb
+++ app/controllers/awards_controller.rb
@@ -1,9 +1,13 @@
 class AwardsController < ApplicationController
   def index
     # FIXME: ¿Qué se supone que mostramos aquí? ¿Las empresas con más adjudicaciones?
-    #@bidders = Bidder.all
+    @bidders = Bidder.all.order(name: :asc)
+
+    @public_bodies = PublicBody.all.order(name: :asc)
 
     @contract_awards = Award.includes(:public_body, :bidder).all.order(amount: :desc)
+
+    @contract_awards_types = Award.select(:process_type).distinct.order(process_type: :asc)
   end
 
   def show
