3020bd77b8098498f5a2f47c1a6ab0046785ec1c
David Cabo <david.cabo@gmail.com>
2016-10-26 21:36:09 +0200

    Amend loading script to use the latest column names #45

--- app/models/award.rb
+++ app/models/award.rb
@@ -5,18 +5,13 @@ class Award < ActiveRecord::Base
   belongs_to :bidder
 
   def self.load_from_hash(properties)
-    # Load the data only if it's a public works award
-    # FIXME: Let's load the whole thing temporarily and use the database to check
-    # whether we're doing it correctly.
-    # return unless properties['[QCLO] Es Obra Pública']=='S'
-
     # Find or create related entities
-    public_body = PublicBody.where(name: properties['Entidad adjudicadora - Organismo']).first_or_create
-    bidder = Bidder.where(name: properties['Formalización del contrato - Contratista']).first_or_create
+    public_body = PublicBody.where(name: properties['[QCLO] Entidad adjudicadora - Nombre']).first_or_create
+    bidder = Bidder.where(name: properties['[QCLO] Contratista - Grupo']).first_or_create
 
     # Read amount and store it in cents. We are being extra careful here not to change any amount
     # even in one cent. That's why we are avoiding floats, not a good fit when exact precision is needed.
-    amount = BigDecimal.new(properties['Análisis - Importe'])*100
+    amount = BigDecimal.new(properties['[QCLO] Importe o canon de adjudicación - Limpio'])*100
 
     # Store the award
     Award.create!({
