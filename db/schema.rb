18dcb321a992719e024f692610905eaa49475bb1
Raúl Díaz Poblete <info@rauldiazpoblete.com>
2016-11-01 19:43:41 +0100

    Add featured field to articles
    In order to show an article in home page

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161027021042) do
+ActiveRecord::Schema.define(version: 20161101165327) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(version: 20161027021042) do
     t.text     "notes"
     t.string   "photo"
     t.boolean  "highlighted",       default: false, null: false
+    t.boolean  "featured",          default: false, null: false
   end
 
   add_index "articles", ["slug"], name: "index_articles_on_slug", unique: true, using: :btree
