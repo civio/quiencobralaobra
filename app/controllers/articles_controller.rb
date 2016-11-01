a7ab7e8b00843cb5e602f3b69287263b731e57f7
Raúl Díaz Poblete <info@rauldiazpoblete.com>
2016-11-01 19:47:13 +0100

    Add gallery in articles show
    Closes #35

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,6 +5,26 @@ class ArticlesController < ApplicationController
     @articles = (can? :manage, Article) ? Article.all : Article.published
     @articles_highlighted = @articles.where( highlighted: true )
     @articles = @articles.where( highlighted: false )
+
+    @gallery_picture = [
+      'accesos-puerto-barcelona.jpg',   
+      'estacion-la-sagrera-barcelona-2.jpg',  
+      'puente-rande-pontevedra.jpg',
+      'acessos-bilbao.jpg',     
+      'hernialde-zizurkil-gipuzkoa.jpg',    
+      'soterramiento-ferrocarril-logrono.jpg',
+      'autovia-regueron-murcia.jpg',    
+      'hospital-cuenca.jpg',      
+      'tanque-gas-puerto-bilbao.jpg',
+      'complejo-ferroviario-valladolid.jpg',  
+      'hospital-guadalajara.jpg',   
+      'tren-aeropuerto-barcelona.jpg',
+      'estacion-atocha-madrid.jpg',   
+      'linea-9-metro-madrid-1.jpg',   
+      'tunel-sorbas-almeria.jpg',
+      'estacion-la-sagrera-barcelona-1.jpg',  
+      'linea-9-metro-madrid-2.jpg'
+    ].sample
   end
 
   def show
