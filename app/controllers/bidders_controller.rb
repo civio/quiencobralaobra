7da5dceda22e53fc9d842bdc22c6de4e22247265
David Cabo <david.cabo@gmail.com>
2016-11-03 06:31:58 +0100

    Implement queries to get awards via UTEs #57

--- app/controllers/bidders_controller.rb
+++ app/controllers/bidders_controller.rb
@@ -7,10 +7,30 @@ class BiddersController < ApplicationController
 
   def show
     @title = @bidder.group
-    @contract_awards = Award.joins(:bidder).where('bidders.group = ?', @bidder.group).order(amount: :desc)
-    @contract_awards_utes = Award.joins(:bidder).where('bidders.group = ?', @bidder.group).order(amount: :desc)
+    @contract_awards = Award.includes(:bidder, :public_body) \
+                          .where(bidders: { group: @bidder.group }) \
+                          .order(amount: :desc)
     @contract_awards_amount = @contract_awards.sum('amount')
-    @contract_awards_utes_amount = @contract_awards_utes.sum('amount')
+
+    # Getting the contracts via not-fully-controlled UTEs is a bit more tricky
+    @contract_awards_utes = Award.joins('INNER JOIN bidders bidder ON awards.bidder_id=bidder.id') \
+                              .joins('LEFT OUTER JOIN ute_companies_mappings ute_mapping ON bidder.name=ute_mapping.ute') \
+                              .includes(:bidder, :public_body) \
+                              .distinct \
+                              .where(ute_mapping: { group: @bidder.group }) \
+                              .where.not(bidder: { group: @bidder.group }) \
+                              .order(amount: :desc)
+                              # distinct: There can be multiple companies of the same group participating
+                              # in the same UTE, so be careful to ask for distinct results to avoid
+                              # getting multiple instances of the same award.
+                              # where.not: UTEs fully belonging to a group are picked in a different place,
+                              # so exclude them here: we just want UTEs where the group participates
+                              # but doesn't fully control
+
+    # We planned to do this to calculate the overall sum:
+    #   @contract_awards_utes_amount = @contract_awards_utes.distinct.sum('amount')
+    # but there's an issue in Rails with 'distinct' sums https://github.com/rails/rails/issues/16791
+    @contract_awards_utes_amount = @contract_awards_utes.map(&:amount).inject(:+)
   end
 
   private
