2e083c79382c3683a2785461d3119f68ad3b4699
Eduardo Sebastian <esebastian@altiora.es>
2016-11-05 18:42:17 +0100

    Add query to distinguish among awarded amounts to bidder groups and UTEs with participation of the bidder groups

--- app/controllers/data_controller.rb
+++ app/controllers/data_controller.rb
@@ -1,6 +1,90 @@
 class DataController < ApplicationController
   
-  # GET /data/grupos-empresariales
+  def grupos_constructores
+    query = <<-EOQ
+      WITH
+      totals_awarded_to_group_companies_or_fully_controlled_utes AS
+      (
+        SELECT
+          bidders.group,
+          bidders.slug,
+          sum(awards.amount) as direct
+        FROM
+          bidders
+            INNER JOIN awards
+              ON bidders.id = awards.bidder_id
+        GROUP BY
+          bidders.group,
+          bidders.slug
+        HAVING
+          bidders.group NOT IN
+          (
+            SELECT
+              ute
+            FROM
+              ute_companies_mappings
+          )
+      ),
+      totals_awarded_to_participated_but_not_fully_controlled_utes AS
+      (
+        WITH
+        awarded_utes_with_group AS
+        (
+          WITH
+          awarded_utes AS
+          (
+            SELECT
+              bidders.group as ute,
+              ute_companies_mappings.company,
+              sum(awards.amount) as sum
+            FROM
+              bidders
+                INNER JOIN awards
+                  ON bidders.id = awards.bidder_id
+                INNER JOIN ute_companies_mappings
+                  ON bidders.group = ute_companies_mappings.ute
+            GROUP BY
+                bidders.group,
+                ute_companies_mappings.company
+          )
+          SELECT
+            awarded_utes.ute,
+            awarded_utes.company,
+            awarded_utes.sum,
+            bidders.group
+          FROM
+            awarded_utes
+            INNER JOIN bidders
+              ON awarded_utes.company = bidders.name
+        )
+        SELECT
+          "group",
+          sum("sum") as on_utes
+        FROM
+          awarded_utes_with_group
+        GROUP BY
+          "group"
+      )
+      SELECT
+        totals_awarded_to_group_companies_or_fully_controlled_utes.group as grupo,
+        slug,
+        direct as importe_grupo,
+        COALESCE(on_utes, 0) as importe_utes,
+        (direct + COALESCE(on_utes, 0)) as total
+      FROM
+        totals_awarded_to_group_companies_or_fully_controlled_utes
+          LEFT JOIN totals_awarded_to_participated_but_not_fully_controlled_utes
+            ON totals_awarded_to_group_companies_or_fully_controlled_utes.group = totals_awarded_to_participated_but_not_fully_controlled_utes.group
+      ORDER BY
+        total DESC
+      LIMIT
+        10;
+    EOQ
+    @results = ActiveRecord::Base.connection.execute(query)
+    render json: @results
+  end
+
+  # GET /data/grupos-constructores
   def empresas
     query = "SELECT 
       properties -> 'Formalización del contrato - Contratista' AS contratista,
