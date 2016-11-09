3d54bef79246dbf51ca6a91850e92ead28a48121
Raúl Díaz Poblete <info@rauldiazpoblete.com>
2016-11-09 11:39:35 +0100

    Remove data/empresas endpoint
    As commented in #82

--- app/controllers/data_controller.rb
+++ app/controllers/data_controller.rb
@@ -85,25 +85,6 @@ class DataController < ApplicationController
     render json: @results
   end
 
-  # GET /data/empresas
-  def empresas
-    query = "SELECT 
-      properties -> 'Formalización del contrato - Contratista' AS contratista,
-      process_type AS procedimiento,
-      SUM(amount) AS importe,
-      COUNT(amount) AS contratos
-    FROM AWARDS
-    WHERE
-      properties -> 'Análisis - Tipo' = 'Obras' AND
-      properties -> 'Formalización del contrato - Contratista' <> '' AND
-      process_type <> '' AND
-      amount IS NOT NULL
-    GROUP BY contratista, procedimiento
-    ORDER BY importe DESC"
-    @results = Award.connection.execute(query)
-    render json: @results
-  end
-
   # GET /data/administraciones
   def administraciones
     query = <<-EOQ
