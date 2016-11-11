9d88a686de02968f718ef1adb66f5bd2c3bbba4f
Raúl Díaz Poblete <info@rauldiazpoblete.com>
2016-11-11 17:52:36 +0100

    Add helper for bidder_group name #58

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+  # render bidder group name from award
+  def render_bidder_group award
+    if award.isUTE?
+      str = 'UTE: '
+      award.getUTEGroups().each_with_index do |group, i|
+        str += i==0 ? '' : ' - '
+        str += link_to group[:name], bidder_path(group[:slug])
+      end
+      return str.html_safe
+    else
+      return link_to award.bidder.group, bidder_path(award.bidder) unless award.bidder.group.blank?
+    end
+  end
 end
